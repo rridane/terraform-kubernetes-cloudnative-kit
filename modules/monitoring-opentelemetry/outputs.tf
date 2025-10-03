54ccdeb6ae0d0e55c9f6518295ad05009fe93543
rridane <rridane@rr-dev.com>
2025-10-03 15:52:04 +0200

    feat: add output

--- modules/monitoring-opentelemetry/outputs.tf
+++ modules/monitoring-opentelemetry/outputs.tf
@@ -1,4 +1,9 @@
-output "bind9_service" {
-  description = "Infos sur le service Bind9"
-  value       = kubernetes_service.bind9
+output "otel_config_yaml" {
+  description = "Config complète générée pour l'OpenTelemetry Collector"
+  value       = local.otel_config_yaml
+}
+
+output "otel_collector_manifest" {
+  description = "Manifeste CRD OpenTelemetryCollector complet"
+  value       = yamlencode(kubernetes_manifest.otel_collector.manifest)
 }
